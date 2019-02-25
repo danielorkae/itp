149cf6fc6b3d7ce3943f1f14a9c9190a0b7e5da8
Daniel Lopes <danielorkae@outlook.com>
2019-02-25 19:59:29 -0300

    move comment on q2

--- list_1/q2.c
+++ list_1/q2.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
+/**
+ * Após lançar um novo modelo de termômetro no mercado,
+ * a Derp Industries descobriu um defeito no seu produto:
+ * ele sempre marca a temperatura com 10 graus acima da
+ * temperatura real!  Como solução emergencial, eles criaram
+ * um novo sistema de display, e pediram que você escreva o
+ * programa para mostrar a temperatura correta.  De quebra,
+ * eles querem adicionar a exibição da temperatura nas escalas
+ * Farenheit e Kelvin (show-off!)
+ */
+
 int main()
 {
-    /**
-     * Após lançar um novo modelo de termômetro no mercado,
-     * a Derp Industries descobriu um defeito no seu produto:
-     * ele sempre marca a temperatura com 10 graus acima da
-     * temperatura real!  Como solução emergencial, eles criaram
-     * um novo sistema de display, e pediram que você escreva o
-     * programa para mostrar a temperatura correta.  De quebra,
-     * eles querem adicionar a exibição da temperatura nas escalas
-     * Farenheit e Kelvin (show-off!)
-     */
 
     int c;
 
